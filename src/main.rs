bd7ec7ae1a1b14dc3712d79450f4252e9b1cdc51
ivan <iv.gurziy@gmail.com>
2019-08-11 20:03:34 +0300

    it seems write now in file

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,47 @@
-use std::io;
 use std::env;
 use std::fs;
 use std::net::Ipv6Addr;
 use std::collections::BinaryHeap;
+use std::fs::File;
+use std::path::Path;
+use std::error::Error;
+use std::io::Write;
 //Return sorted ipv6 addresses as source use file
 //temporary util get two args, then will be added proper process of second argument 
+
+fn help() {
+println!("usage:
+	ipv6_sort input_file output_file
+description: utility sort input file of ipv6 addresses and return result in stdout or output file
+    ");
+
+}
+
+struct Config {
+    input_file: String,
+    output_file: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let input_file = args[1].clone();
+    let output_file = args[2].clone();
+
+    Config {input_file, output_file}
+}
+
+
 fn main() {
-	let mut sort_heap = BinaryHeap::new();
-
 	let args: Vec<String> = env::args().collect();
-	let input_file = &args[1];
-	let output_file = &args[2];
+	let config = parse_config(&args);
 
-	let contents = fs::read_to_string(input_file)
+	let mut sort_heap = BinaryHeap::new();
+	let contents = fs::read_to_string(config.input_file)
 	           .expect("Something went wrong reading the file");
 
-	for line in contents.lines() {
-		let ipv6_addr: Ipv6Addr = match line.trim().parse(){
+
+for line in contents.lines() {
+
+let ipv6_addr: Ipv6Addr = match line.trim().parse(){
                   Ok(ipv6_addr) => ipv6_addr,
                   Err(_) => continue,
                 };
@@ -24,7 +49,29 @@ fn main() {
 		sort_heap.push(ipv6_in_u128);
    }
    let sort_vec = sort_heap.into_sorted_vec();
-   for num in sort_vec {
-       println!("{:?}", Ipv6Addr::from(num));
-	   }
+		match config.output_file.as_str() {
+		"stdout" =>  for num in sort_vec {
+       println!("{:?}", Ipv6Addr::from(num))
+
+},
+
+_ => {for num in sort_vec {
+			let path = Path::new(&config.output_file);
+			let display = &path.display();
+
+    // Open a file in write-only mode, returns `io::Result<File>`
+    let mut file = match File::create(&config.output_file) {
+        Err(why) => panic!("couldn't create {}: {}", &display, why.description()),
+        Ok(file) => file,
+
+};
+
+    match file.write_all(Ipv6Addr::from(num).to_string().as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", &display, why.description()),
+        Ok(_) => println!("successfully wrote to {}", &display),
+
+}
+	   }},
+
+}
 }
